c1ee9c9b8751e6450abf64821ec200bcd833defa
Chris Franklin <cf73021@gmail.com>
2019-03-01 04:12:40 +0000

    Automatically backed up by Learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,7 +1,7 @@
 class Spaceship {
-  constructor(name, crew, phasers, shields){
-    this.name = name;
-    this.crew = crew;
+  constructor(name, crew, phasers, shields) {
+    this.name = name ;
+    this.crew = crew ;
     if (!!crew) {
       this.crew.forEach (function(element){
         element.currentShip = this ;
@@ -12,7 +12,7 @@ class Spaceship {
     this.phasersCharge = "uncharged" ;
     this.warpDrive = "disengaged" ;
     this.cloaked = false ;
-    if (!this.crew) {this.docked = true};
-    else {this.docked = false};
+    if (!this.crew) {this.docked = true}
+    else {this.docked = false}
   }
 }
