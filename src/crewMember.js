bdc54072829acdab86520cab08125575a1fce191
Chris Franklin <cf73021@gmail.com>
2019-03-01 04:18:02 +0000

    Automatically backed up by Learn

--- src/crewMember.js
+++ src/crewMember.js
@@ -3,12 +3,11 @@ class CrewMember {
     this.position = position ;
     this.currentShip = "Looking for a Rig."
   }
-  engageWarpDrive() {
-    if (this.currentShip !== "Looking for a Rig") {
+  engageWarpDrive(){
+    if (this.position === "Pilot" && this.currentShip !== "Looking for a Rig.") {
+      this.currentShip.warpDrive = "engaged"
     }
-    else {
-      return "had no effect";
-    }
+    else {return "had no effect"}
   }
   chargePhasers(){
     if (this.position === "Gunner" && this.currentShip !== "Looking for a Rig.") {
